540d46c14b29fd69131b66b17467c283ef993399
swaswa14 <129421068+swaswa14@users.noreply.github.com>
2023-05-29 19:55:42 +0800

    first commit for back-end

---
+++ back-end/src/main/java/ph/cdo/backend/BackEndApplication.java
@@ -0,0 +1,13 @@
+package ph.cdo.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/ph/cdo/backend/BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package ph.cdo.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
